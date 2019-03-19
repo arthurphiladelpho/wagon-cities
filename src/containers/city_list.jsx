9ff4791733cff28ed72439ebb1494128d818b427
Arthur Philadelpho <arthur22rj@gmail.com>
2019-03-19 15:19:59 -0300

    Convert React app into Redux.

--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import City from './city';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { setCities } from '../actions';
 
 class CityList extends Component {
+
+  componentWillMount() {
+    this.props.setCities();
+  }
+
   render() {
     return (
-      <div className="city-list col-sm-7">
+      <div className="city-list col-sm-4">
         { this.props.cities.map((city) => {
           return (
             <City key={city.name} city={city} />
@@ -15,4 +23,12 @@ class CityList extends Component {
   }
 }
 
-export default CityList;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({setCities: setCities}, dispatch);
+}
+
+function mapStateToProps(state) {
+  return { cities: state.cities };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CityList);
